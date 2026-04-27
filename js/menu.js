a3b5d90fe0449db2bc48526c7f207f2509f558d9
Jose Sanchez <jose.jair.ruiz@gmail.com>
2026-04-27 11:38:02 +1000

    feat: implement cache busting for menu data and script imports

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('data/menu.xml')
+    fetch('data/menu.xml?v=' + new Date().getTime())
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok ' + response.statusText);
